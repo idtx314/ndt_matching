aa24108c1263656fb3ea8f3d605e0ab3ba51659d
Ian Davis <idtx314business@gmail.com>
2019-07-30 13:54:39 -0500

    Added message headers to ndt_lib. Compilation error: string.hpp No such file or directory. Added message dependencies to ament_target_dependencies for ndt_node and ndt_lib. Compiling as expected.

--- src/ndt_lib.cpp
+++ src/ndt_lib.cpp
@@ -2,6 +2,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "std_msgs/msg/string.hpp"
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+
 namespace ndt_matching
 {
 
